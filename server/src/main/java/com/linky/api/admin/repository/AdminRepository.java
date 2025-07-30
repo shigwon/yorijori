c80df91eef59ab08ae82f1dadf1b70903fc495da
신규원 <skdltmrbdnjs@naver.com>
2025-07-30 14:33:29 +0900

    feat: 대시보드 시간별, 요일별 조회 기능 추가 25-07-30 1. 시간 별 주문 건 수 조회 기능 추가 2. 요일 별 주문 건 수 조회 기능 추가

--- server/src/main/java/com/linky/api/admin/repository/AdminRepository.java
+++ server/src/main/java/com/linky/api/admin/repository/AdminRepository.java
@@ -5,6 +5,8 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface AdminRepository {
@@ -12,4 +14,8 @@ public interface AdminRepository {
     Admin findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
     int getOrderCountByPeriod(@Param("startDateTime") LocalDateTime startDateTime,
                               @Param("endDateTime") LocalDateTime endDateTime);
+    List<Map<String, Object>> getHourlyOrderCountInDay(@Param("startDateTime") LocalDateTime startDateTime,
+                                                  @Param("endDateTime") LocalDateTime endDateTime);
+    List<Map<String, Object>> getDailyOrderCountInWeek(@Param("startDateTime") LocalDateTime startDateTime,
+                                                       @Param("endDateTime") LocalDateTime endDateTime);
 }
