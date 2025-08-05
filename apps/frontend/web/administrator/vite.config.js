0710f66e47285484b29e26c12c34965f84fbdb0d
jiwon <wkdska1590@naver.com>
2025-08-05 15:22:23 +0900

    feat : Vite 서버 포트 및 WebRTC 프록시 설정 추가
    25-08-05
    1. Vite dev 서버 포토를 3000으로 지정
    2. WebRTC 토큰 발급용 API 요청을 위한 프록시 설정
    3. @ 경로 별칭을 src로 설정하여 경로 가독성 향상
    4. vue-devtools 플러그인 추가로 개발 편의성 강화

--- apps/frontend/web/administrator/vite.config.js
+++ apps/frontend/web/administrator/vite.config.js
@@ -1,7 +1,35 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api/v1/stream/':{                     // webrtc 발급용 api
+        target: 'http://192.168.100.83:8080',
+        changeOrigin: true,
+        secure: false
+      },
+      '/api': {
+        target: 'http://192.168.100.115:8080', // 백엔드 서버 주소
+        changeOrigin: true,
+        secure: false,
+        ws: true, // 웹소켓 프록시 설정
+      },
+      
+    }
+  }
 })
