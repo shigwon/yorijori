cef3e070f28683a01214f9494968669a54cd0c93
신규원 <skdltmrbdnjs@naver.com>
2025-08-07 15:36:41 +0900

    feat: RobotLocation 관련 PostgreSQL config 파일 추가 25-08-07 1. PostgreSQLConfig 작성

---
+++ apps/backend/src/main/java/com/linky/config/database/PostgreSQLConfig.java
@@ -0,0 +1,92 @@
+package com.linky.config.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.linky.api.robot.repository",
+        entityManagerFactoryRef = "postgresEntityManagerFactory",
+        transactionManagerRef = "postgresTransactionManager",
+        includeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                value = RobotLocationHistoryRepository.class)
+)
+public class PostgreSQLConfig {
+
+    @Value("${spring.datasource.postgres.url}")
+    private String postgresUrl;
+
+    @Value("${spring.datasource.postgres.username}")
+    private String postgresUsername;
+
+    @Value("${spring.datasource.postgres.password}")
+    private String postgresPassword;
+
+    @Value("${spring.datasource.postgres.driver-class-name}")
+    private String postgresDriverClassName;
+
+    @Bean(name = "postgresDataSource")
+    public DataSource postgresDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(postgresUrl);
+        config.setUsername(postgresUsername);
+        config.setPassword(postgresPassword);
+        config.setDriverClassName(postgresDriverClassName);
+
+        config.setMinimumIdle(5);
+        config.setMaximumPoolSize(20);
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
+        config.setPoolName("PostgreSQL-HikariPool");
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean(name = "postgresEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean postgresEntityManagerFactory(
+            @Qualifier("postgresDataSource") DataSource dataSource,
+            JpaProperties jpaProperties,
+            HibernateProperties hibernateProperties) {
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setPackagesToScan("com.linky.api.robot.entity");
+        factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Map<String, Object> properties = hibernateProperties.determineHibernateProperties(
+                jpaProperties.getProperties(), new HibernateSettings());
+
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+
+        factory.setJpaPropertyMap(properties);
+        return factory;
+    }
+
+    @Bean(name = "postgresTransactionManager")
+    public PlatformTransactionManager postgresTransactionManager(
+            @Qualifier("postgresEntityManagerFactory") LocalContainerEntityManagerFactoryBean factory) {
+
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(factory.getObject());
+        return transactionManager;
+    }
+}
