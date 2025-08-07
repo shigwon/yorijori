4a44565e5b4c7bbd7689ce86af7635ff464d50d4
신규원 <skdltmrbdnjs@naver.com>
2025-08-07 16:21:53 +0900

    feat: 메인 클래스 스케줄링 활성화 25-08-07 1. LinkyApplication 메인 클래스 수정

--- apps/backend/src/main/java/com/linky/LinkyApplication.java
+++ apps/backend/src/main/java/com/linky/LinkyApplication.java
@@ -3,8 +3,10 @@ package com.linky;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LinkyApplication {
 
     public static void main(String[] args) {
