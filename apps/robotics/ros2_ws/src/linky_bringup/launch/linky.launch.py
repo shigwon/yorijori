40f97430fba99d103c423a03b99a93beb737881e
유준호 <juno980220@naver.com>
2025-08-10 21:36:05 +0900

    Feat: ROS2 Main Node 개발중 2025-08-10 1. 로봇 제어 및 통신 주관하는 메인 노드 생성 2. 서보 제어 3. 주행 제어 4. 고객 정보 관리 5. 배달, 로봇 상태 관리

--- apps/robotics/ros2_ws/src/linky_bringup/launch/linky.launch.py
+++ apps/robotics/ros2_ws/src/linky_bringup/launch/linky.launch.py
@@ -8,34 +8,21 @@ from launch_ros.substitutions import FindPackageShare
 
 import os
 
-def generate_launch_description():
-    parameters=[{
-          'frame_id':'camera_link',
-          'subscribe_depth':True,
-          'subscribe_odom_info':True,
-          'approx_sync':False,
-          'wait_imu_to_init':True}]
-
-    remappings=[
-          ('imu', '/imu/data'),
-          ('rgb/image', '/camera/infra1/image_rect_raw'),
-          ('rgb/camera_info', '/camera/infra1/camera_info'),
-          ('depth/image', '/camera/depth/image_rect_raw')]
-    
+def generate_launch_description():    
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 PathJoinSubstitution([
                     FindPackageShare('realsense2_linky'),
                     'launch',
-                    'realsense2_linky.py'
+                    'realsense2_linky.launch.py'
                 ])
             )
         ),
 
         Node(
             package='face_recognition_pkg',
-            executable='face_recognition_server_node',
+            executable='face_db_matcher_node',
             name='face_recognition',
             output='screen'
         ),
